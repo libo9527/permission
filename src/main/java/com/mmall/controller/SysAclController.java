184610c8aaf4b7fd7d967e99a2b1cf4f5426f9e8
libo <18629374709@163.com>
2018-11-24 14:48:27 +0800

    Interface of save and update at permission point module.

---
+++ src/main/java/com/mmall/controller/SysAclController.java
@@ -0,0 +1,39 @@
+package com.mmall.controller;
+
+import com.mmall.common.JsonData;
+import com.mmall.param.AclParam;
+import com.mmall.service.SysAclService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * @Description：
+ * @Auther： libo
+ * @date： 2018/11/24:14:35
+ */
+@Slf4j
+@Controller
+@RequestMapping("/sys/acl")
+public class SysAclController {
+
+    @Resource
+    private SysAclService sysAclService;
+
+    @RequestMapping("/save.json")
+    @ResponseBody
+    public JsonData saveAclModule(AclParam param) {
+        sysAclService.save(param);
+        return JsonData.success();
+    }
+
+    @RequestMapping("/update.json")
+    @ResponseBody
+    public JsonData updateAclModule(AclParam param) {
+        sysAclService.update(param);
+        return JsonData.success();
+    }
+}
---
+++ src/main/java/com/mmall/service/SysAclService.java
@@ -0,0 +1,73 @@
+package com.mmall.service;
+
+import com.google.common.base.Preconditions;
+import com.mmall.common.RequestHolder;
+import com.mmall.dao.SysAclMapper;
+import com.mmall.exception.ParamException;
+import com.mmall.model.SysAcl;
+import com.mmall.param.AclParam;
+import com.mmall.util.BeanValidator;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @Description：
+ * @Auther： libo
+ * @date： 2018/11/24:14:36
+ */
+@Service
+public class SysAclService {
+
+    @Resource
+    private SysAclMapper sysAclMapper;
+
+    public void save(AclParam param) {
+        BeanValidator.check(param);
+        if (checkExist(param.getAclModuleId(), param.getName(), param.getId())) {
+            throw new ParamException("当前权限模块下面存在相同名称的权限点");
+        }
+        SysAcl acl = SysAcl.builder()
+                .name(param.getName())
+                .aclModuleId(param.getAclModuleId())
+                .url(param.getUrl())
+                .type(param.getType())
+                .status(param.getStatus())
+                .seq(param.getSeq())
+                .remark(param.getRemark())
+                .build();
+        acl.setCode(generateCode());
+        acl.setOperator("system"); // TODO:
+        acl.setOperateIp("127.0.0.1"); // TODO:
+        acl.setOperateTime(new Date());
+        sysAclMapper.insertSelective(acl);
+    }
+
+    public void update(AclParam param) {
+        BeanValidator.check(param);
+        if (checkExist(param.getAclModuleId(), param.getName(), param.getId())) {
+            throw new ParamException("当前权限模块下面存在相同名称的权限点");
+        }
+        SysAcl before = sysAclMapper.selectByPrimaryKey(param.getId());
+        Preconditions.checkNotNull(before, "待更新的权限点不存在");
+
+        SysAcl after = SysAcl.builder().id(param.getId()).name(param.getName()).aclModuleId(param.getAclModuleId()).url(param.getUrl())
+                .type(param.getType()).status(param.getStatus()).seq(param.getSeq()).remark(param.getRemark()).build();
+        after.setOperator("system"); // TODO:
+        after.setOperateIp("127.0.0.1"); // TODO:
+        after.setOperateTime(new Date());
+
+        sysAclMapper.updateByPrimaryKeySelective(after);
+    }
+
+    private boolean checkExist(int aclModuleId, String name, Integer id) {
+        return sysAclMapper.countByNameAndAclModuleId(aclModuleId, name, id) > 0;
+    }
+
+    private String generateCode() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        return dateFormat.format(new Date()) + "_" + (int) (Math.random() * 100);
+    }
+}
